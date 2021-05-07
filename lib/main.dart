eac86eb284048407c82dd1021ec79eaf7e5c1117
Irvit1 <irvit2002gupta@gmail.com>
2021-05-07 22:38:43 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:covid_tracker/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+      ),
+      home: HomePage(),
+    );
+  }
+}
