1f3535f7040c53c02f4915d8cea097271bca6a59
sumair1909 <sumairbais123@gmail.com>
2021-05-08 23:39:45 +0530

    list view added

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:google_fonts/google_fonts.dart';
 
 class homepage extends StatefulWidget {
   @override
@@ -70,15 +71,55 @@ class _homepageState extends State<homepage> {
                                   image: AssetImage("assets/person1.png"),
                                 ),
                               ),
-                            )
+                            ),
+
                             // Image.asset("assets/person.png"),
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
-              )
+              ),
+              SizedBox(height: 15),
+              Container(
+                  child: Column(children: [
+                Text("INDIA",
+                    style: GoogleFonts.lato(
+                        fontSize: 30, fontWeight: FontWeight.bold)),
+                GridView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, childAspectRatio: 2),
+                  children: <Widget>[
+                    StatusPanel(
+                      title: 'CONFIRMED',
+                      panelColor: Colors.red[100],
+                      textColor: Colors.red,
+                      count: '100',
+                    ),
+                    StatusPanel(
+                      title: 'ACTIVE',
+                      panelColor: Colors.blue[100],
+                      textColor: Colors.blue[900],
+                      count: '100',
+                    ),
+                    StatusPanel(
+                      title: 'RECOVERED',
+                      panelColor: Colors.green[100],
+                      textColor: Colors.green,
+                      count: '100',
+                    ),
+                    StatusPanel(
+                      title: 'DEATHS',
+                      panelColor: Colors.grey[400],
+                      textColor: Colors.grey[900],
+                      count: '10',
+                    ),
+                  ],
+                ),
+              ]))
             ],
           ),
         ),
@@ -102,5 +143,48 @@ class MyClipper extends CustomClipper<Path> {
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
     return false;
+  }
+}
+
+class StatusPanel extends StatelessWidget {
+  final Color panelColor;
+  final Color textColor;
+  final String title;
+  final String count;
+
+  const StatusPanel(
+      {Key key, this.panelColor, this.textColor, this.title, this.count})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.black),
+                      borderRadius: BorderRadius.circular(20)),
+        margin: EdgeInsets.all(5),
+        height: 80,
+        width: width / 2,
+        color: panelColor,
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+        title,
+        style: TextStyle(
+      fontWeight: FontWeight.bold, fontSize: 16, color: textColor),
+        ),SizedBox(height:10),
+        Text(
+        count,
+        style: TextStyle(
+      fontSize: 16, fontWeight: FontWeight.bold, color: textColor),
+        )
+      ],
+        ),
+        ),
+    );
   }
 }
